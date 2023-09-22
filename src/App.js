b3574b8a0d67dbcf02038b5dd3f430dbe3c26ba4
Bilal Hassen <bilalhassen097@gmail.com>
2023-09-21 20:42:04 -0400

    add in components and basic styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>bilal tester</h1>
+    </div>
+  );
 }
 
 export default App;
